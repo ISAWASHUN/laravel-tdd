81957b831159ae04a339b45e2ba724325853b70c
ISAWASHUN <syun20000616@icloud.com>
2024-04-02 00:15:19 +0900

    環境構築

---
+++ app/Models/VacancyLevel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+class VacancyLevel
+{
+    private $remainingCount;
+
+    public function __construct(int $remainingCount)
+    {
+        $this->remainingCount = $remainingCount;
+    }
+
+    public function mark(): string
+{
+    if ($this->remainingCount === 0) {
+        return '×';
+    }
+    if ($this->remainingCount < 5) {
+        return '△';
+    }
+    return '◎';
+}
+
+public function __toString()
+{
+    return $this->mark();
+}
+
+
+}
